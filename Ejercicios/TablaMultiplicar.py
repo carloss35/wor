c0ce7c11b4532124332f7a8bf7a3a362c56eeec6
carloss35 <carlopher4@alu.edu.gva.es>
2024-02-14 13:37:22 +0100

    14-02-2024

---
+++ Ejercicios/TablaMultiplicar.py
@@ -0,0 +1,4 @@
+Tabla_multiplicar = int(input("dime el numero parar calcular la tabla de multiplicar: "))
+for mul_factor in range(1, 10):
+        result = Tabla_multiplicar * mul_factor
+        print(f'{Tabla_multiplicar} * {mul_factor} = {result}')

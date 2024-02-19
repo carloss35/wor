430253475ce67160c06546de78d6e84d4aa2d801
carloss35 <carlopher4@alu.edu.gva.es>
2024-02-19 10:30:53 +0100

    Calculadora finalizada

--- Ejercicios/calculadora.py
+++ Ejercicios/calculadora.py
@@ -1,17 +1,19 @@
+def calculadora(num1,num2, oper):
+    if operacion == "+":
+        resultado = num1 + num2 
+    elif operacion == "-":
+        resultado = num1 - num2 
+    elif operacion == "*":
+        resultado = num1 * num2 
+    else:
+        resultado = num1 / num2 
+    return resultado
+
 valor1 = int(input('Introduce el primer numero: '))
 valor2 = int(input('Introduce el segundo numero: '))
 operacion = (input('Introduce la operación aritmética (+, -, * o /): '))
-while operacion != "+" or "-" or "*" or "/":
+while operacion not in  ("+" , "-" , "*", "/"):
     print("Debes introducir una operación correcta, vuelve a intentarlo")
     operacion = input("Introduce la operación aritmética (+, -, * o /): ")
-    
-if operacion == "+":
-    resultado_final = valor1 + valor2 
-elif operacion == "-":
-    resultado_final = valor1 - valor2 
-elif operacion == "*":
-    resultado_final = valor1 * valor2 
-else:
-    resultado_final = valor1 / valor2 
 
-print(f'El resultado de la operación aritmética de {valor1} {operacion} {valor2} es: {resultado_final}')
+print(f'El resultado de la operación aritmética de {valor1} {operacion} {valor2} es: {calculadora(valor1,valor2,operacion)}')

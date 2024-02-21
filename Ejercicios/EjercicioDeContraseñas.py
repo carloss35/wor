0096f2c77f55c2281a12c996d71635a24bbd3bde
carloss35 <carlopher4@alu.edu.gva.es>
2024-02-21 13:55:13 +0100

    Ejercicios contraeñas y calculadora

---
+++ Ejercicios/EjercicioDeContraseñas.py
@@ -0,0 +1,5 @@
+#El programa debería contener:
+    #Al menos 4 funciones que mezclen texto.
+    #Pedir al usuario que introduzca una palabra
+    #Generar un número aleatorio entre 1 y 3
+    #Llamar a las funciones de mezcla de texto con diferentes combinaciones para cada posible número aleatorio (1, 2 o 3).
